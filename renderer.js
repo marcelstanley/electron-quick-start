0130b4d790c98316aa425d58db0bb95af359a123
Marcel Moura <marcel.stanley@gmail.com>
2021-08-12 17:07:42 -0300

    Log message to console when a button is clicked
    A CSP warning is expected to be logged in the console upon application start. See:
    - https://stackoverflow.com/questions/65445255/content-security-policy-of-your-site-blocks-the-use-of-eval-in-javascript-wa
    - https://github.com/electron/electron/issues/26973

--- renderer.js
+++ renderer.js
@@ -1,6 +1,7 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+function logMessage() {
+    console.log('Called logMessage()')
+}
+
+document.getElementById('logMessage').addEventListener('click', () => {
+    logMessage()
+})
